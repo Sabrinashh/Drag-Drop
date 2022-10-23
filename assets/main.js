74954364935fc98edb65b6f389c08e3f05e33b0b
Sabrinashh <110469877+Sabrinashh@users.noreply.github.com>
2022-10-23 13:09:41 +0400

    file

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+let dragItem;
+let items = document.querySelectorAll('.item');
+let areas = document.querySelectorAll('.area');
+for (const item of items) {
+    item.addEventListener('dragstart', ()=>{
+        dragItem = item;
+    })
+}
+for (const area of areas) {
+    area.addEventListener('dragover',e=>{
+        e.preventDefault();
+    })
+    area.addEventListener('drop',()=>{
+        area.appendChild(dragItem)
+    })
+}
